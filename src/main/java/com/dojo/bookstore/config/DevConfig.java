201f83aaf90447cd54a39a30dfe74e1c32e6d363
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-20 18:06:20 -0300

    criando ambientes funcionais de Teste(local) e Dev

---
+++ src/main/java/com/dojo/bookstore/config/DevConfig.java
@@ -0,0 +1,50 @@
+package com.dojo.bookstore.config;
+
+import com.dojo.bookstore.entities.Book;
+import com.dojo.bookstore.entities.Category;
+import com.dojo.bookstore.repositories.BookRepository;
+import com.dojo.bookstore.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("dev")
+public class DevConfig implements CommandLineRunner {
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Autowired
+    private CategoryRepository categoriaRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if(strategy.equals("create")) {
+            Category cat1 = new Category(null, "Informatica", "livro de TI");
+            Category cat2 = new Category(null, "Ficção-Cientifica", "livro de ficção-cientifica");
+            Category cat3 = new Category(null, "Biografias", "livro de Biografias");
+
+            Book livro1 = new Book(null, "Clean Code", "Robert Martin", "xpto", cat1);
+            Book livro2 = new Book(null, "Engenharia de software", "Louis V.Gerstner", "xpto", cat1);
+            Book livro3 = new Book(null, "A Máquina do tempo", "H. G. Wells", "xpto", cat2);
+            Book livro4 = new Book(null, "Guerras dos mundos", "H. G. Wells", "xpto", cat2);
+            Book livro5 = new Book(null, "Eu, Robô", "Isaac Asimov", "xpto", cat2);
+
+            cat1.getBooks().addAll(Arrays.asList(livro1, livro2));
+            cat2.getBooks().addAll(Arrays.asList(livro3, livro4, livro5));
+
+            this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+            this.bookRepository.saveAll(Arrays.asList(livro1, livro2, livro3, livro4, livro5));
+        }
+
+    }
+}
