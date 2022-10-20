7e3f1861315c77f83a798c9a9f460053ef95b2bf
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-10-19 23:01:58 -0300

    ajustes no H2-Database

--- src/main/java/com/dojo/bookstore/BookstoreApplication.java
+++ src/main/java/com/dojo/bookstore/BookstoreApplication.java
@@ -1,14 +1,39 @@
 package com.dojo.bookstore;
 
+import com.dojo.bookstore.entities.Book;
+import com.dojo.bookstore.entities.Category;
+import com.dojo.bookstore.repositories.BookRepository;
+import com.dojo.bookstore.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoriaRepository;
+
+	@Autowired
+	private BookRepository bookRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null,"Informatica","livro de TI");
+
+		Book livro1 = new Book(null,"Clean Code","Robert Martin","xpto",cat1);
+
+		cat1.getBooks().addAll(Arrays.asList(livro1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.bookRepository.saveAll(Arrays.asList(livro1));
+
+	}
 }
